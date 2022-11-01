42ad3003a222bc09ffac00d5611ca8616fa8d12e
Vidyasagar v <97279970+VIDYASAGARVV@users.noreply.github.com>
2022-11-01 13:14:41 +0530

    Create site.php

---
+++ site.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+
+<?php
+$title ="My First Post";
+$author ="Sagar";
+$wordCount=400;
+include "article-header.php"
+ ?>
+ <?php
+include "useful-tools.php";
+echo $feetInMile;
+  ?>
+ <p>hello world<p>
+
+
+  </body>
+</html>
